aa2dbccd1053a20f00ed92511bdaed728d830afa
IAmLev1 <levisj.aguiar@udea.edu.co>
2024-08-10 16:39:21 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Inicio } from "./paginas/Inicio"
-import './styles.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Inicio } from "./paginas/Inicio";
+import "./styles.css";
 import Login from "./paginas/Login";
 import Registro from "./paginas/Registro";
-
+import InicioAdmin from "./paginas/administracion/InicioAdmin";
+import { Estudiantes } from "./paginas/Estudiantes";
 
 function App() {
   //
   return (
     <div>
       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Inicio/>}/>
-            <Route path="/Login" element={<Login/>}/>
-            <Route path="/Register" element={<Registro/>}/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Registro />} />
+          <Route path="/Admin" element={<InicioAdmin />} />
+          <Route path="/Estudiantes" element={<Estudiantes />} />
+        </Routes>
       </BrowserRouter>
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
